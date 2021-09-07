dd73bf88f2dc77aea97668661df7a3677d018a8e
nmarrocchi <nmarrocchi@la-providence.net>
2021-09-07 11:39:53 +0200

    Add Login Page & basic CSS

--- index.php
+++ index.php
@@ -4,9 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
+    <link rel="icon" href="img/Icon.png">
+    <link rel="stylesheet" href="css/style.css">
+    <title>TP1</title>
 </head>
 <body>
+<?php $LoginValid = "";?>
+    <div class="L_Form">
+        <form action="Login.php" method="post">
+                <b class='LoginValid'><?php echo $LoginValid ?></b>
+                <label for="Username">Username</label>
+                <p><input type="text" placeholder="Username" name="Username"></p>
+                <label for="Password">Password</label>
+                <p><input type="password" placeholder="Password" name="Password"></p>
+               <p><input type="submit" name='submit' value="Login"></p>
+        </form>
+    </div>
     
 </body>
 </html>
