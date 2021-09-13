d7d282358291092935d3b22feaa4a56aec28c0a5
nmarrocchi <nmarrocchi@la-providence.net>
2021-09-13 16:13:54 +0200

    update CSS login fom

---
+++ accueil.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+include "functions.php";
+
+$LoginValid = "";
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="CSS/style.css" type="text/css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+</head>
+<body>
+
+  <div class="Contenu">
+
+
+    </div>
+    
+</body>
+</html>
