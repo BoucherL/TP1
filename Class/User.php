5d356454608d203fc73ed811712e7e3674d982ab
nmarrocchi <nmarrocchi@la-providence.net>
2021-09-14 10:14:04 +0200

    session

--- Class/User.php
+++ Class/User.php
@@ -23,23 +23,7 @@
         }*/
 
         public function AfficheLoginForm(){
-            ?>
-
-                <form action="index.php" method="post">
-
-                    <h1>Connexion</h1>
-                    <b class='LoginValid'><?php echo $LoginValid ?></b>
-
-                    <label><b>Nom d'utilisateur</b></label>
-                    <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
-
-                    <label><b>Mot de passe</b></label>
-                    <input type="password" placeholder="Entrer le mot de passe" name="password" required>
-
-                    <input type="submit" class='submit' value='Login' >
-                </form>
-
-            <?php
+            echo "coucou";
         }
 
         public function Connexion(){
