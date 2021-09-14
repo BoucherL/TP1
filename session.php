6f51d63e4c3954a40b06492b972cf6c0b9171582
nmarrocchi <nmarrocchi@la-providence.net>
2021-09-14 09:32:47 +0200

    session

--- session.php
+++ session.php
@@ -1,6 +1,6 @@
 <?php
     include "Class/User.php";
-
+echo 'coucou';
     // - connexion à la bdd
     try{
         $BDD = new PDO('mysql:host=192.168.64.204; dbname=TP1; charset=utf8', "admin" , "admin" );
@@ -10,7 +10,7 @@
         echo "J'ai eu un problème erreur :".$e->getMessage();
         }
 
-echo 'coucou';
+
 
 
 ?>
