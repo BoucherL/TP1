bc2aff1a70f799d47ccca2badb53239d8c1621dd
nmarrocchi <nmarrocchi@la-providence.net>
2021-09-14 11:56:17 +0200

    login form

--- Class/User.php
+++ Class/User.php
@@ -45,6 +45,7 @@
                 if ($exist["COUNT(*)"] > 0) {
                     //$this->Connexion($user, $passwd);                        
                     $admin = $this->_bdd->query("SELECT `IsAdmin` FROM `user` WHERE user ='".$user."'");
+                    print_r("SELECT `IsAdmin` FROM `user` WHERE user ='".$user."'");
                     $admin = $admin->fetch();
                     echo $admin;
                 }
