563fc7bb1eb974c25727cf195e06714606165a77
nmarrocchi <nmarrocchi@la-providence.net>
2021-09-14 09:04:54 +0200

    login function class user

--- index.php
+++ index.php
@@ -1,15 +1,20 @@
 <?php
-   // session_start();
+    //session_start();
 ?>
 <!DOCTYPE html>
 <html lang="fr">
     <head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="CSS/style.css" type="text/css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="CSS/style.css" type="text/css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Accueil</title>
     </head>
     <body>
-        <?php echo "coucou" ?>
+        <?php
+            include "session.php";
+            if($access != true){
+                $access = $UserLog->SeDeconnecter();
+            }
+        ?>
     </body>
 </html>
