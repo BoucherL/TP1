af4b07079c3eeaa3d06196754cc402b1259807a3
nmarrocchi <nmarrocchi@la-providence.net>
2021-09-14 09:16:18 +0200

    login function class user

--- session.php
+++ session.php
@@ -2,16 +2,6 @@
 
 include "Class/User.php";
 
-// - Gestion BDD
-$errorMessage="";
-
-try{
-    $user = "admin";
-    $pass = "admin";
-    $BDD = new PDO('mysql:host=192.168.64.204;dbname=TP1', $user, $pass);
-}catch(Exception $e){
-    $errorMessage .= $e->getMessage();
-}
 
 echo "coucou";
 
